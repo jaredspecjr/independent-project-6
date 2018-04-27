fbeb6bdd58bb72785a906eadcd6d0c02833be7c1
jared <jaredspecjr@gmail.com>
2018-04-27 10:02:35 -0700

    tried to make an api call and failed not sure why

--- src/main.js
+++ src/main.js
@@ -6,12 +6,13 @@ import './styles.css';
 
 $(document).ready(function(){
   $("#doctorSearch").click(function(){
+    debugger;
     let search = $("#search").val(); //changed location to search
     $("#search").val("");
     let promise = new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url = ``;
-      request.onload = fucntion(){
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${search}&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
         if (this.status === 200){
           resolve(request.response);
         } else {
@@ -22,9 +23,10 @@ $(document).ready(function(){
     });
 
     promise.then(function(response){
-      body = JSON.parse(response);
+      let body = JSON.parse(response);
+      $(".showDoctors").append(`${body.data.profile.first_name}`)
     }, function(error){
-
+      $(".showErrors").text(`There was an error processing your request: ${error.message}`);
     });
   });
 });
