f39f85c3ec228514c50aef7d4ed51b700a89b561
jared <jaredspecjr@gmail.com>
2018-04-27 08:54:05 -0700

    add imports and  html, ran a test to see if webpack is working correctly

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+// import {} from './businessLogic.js';
+
 $(document).ready(function(){
   $("#doctorSearch").click(function(){
     let city = $("#location").val();
