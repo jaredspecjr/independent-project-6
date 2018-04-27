c41877ced96f2fb0ad05981b3c892ffa05a15b9e
jared <jaredspecjr@gmail.com>
2018-04-27 16:45:14 -0700

    seperate logic

---
+++ src/BusinessLogic.js
@@ -0,0 +1,20 @@
+class DoctorApi{
+  constructor(){}
+
+  doctorPromise(name, search){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
+        if (this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("get", url, true);
+      request.send();
+    });
+  }
+}
+export {DoctorApi}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import {} from './businessLogic.js';
+import {DoctorApi} from './businessLogic.js';
 
 $(document).ready(function(){
   $("#doctorSearch").click(function(){
@@ -10,19 +10,9 @@ $(document).ready(function(){
     let name = $("#name").val();
     $("#search").val("");
     $("#name").val("");
-    let promise = new Promise(function(resolve, reject){
-      let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${search}&skip=2&limit=10&user_key=${process.env.exports.apiKey}`;
-      request.onload = function(){
-        if (this.status === 200){
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("get", url, true);
-      request.send();
-    });
+    let doctorApi = new DoctorApi();
+    let promise = doctorApi.doctorPromise(name, search);
+
 
     promise.then(function(response){
       let body = JSON.parse(response);
@@ -31,17 +21,13 @@ $(document).ready(function(){
           $(".showDoctors").append("Sorry there are no results, Please try another search" + "<br>");
         }else if(body.data[i].practices.length === 0){
           i--; i++; //this subtracts the index then adds it again so that the loop doesnt get disturbed and can continue
-
         } else {
-          console.log(body.data[i]);
-          console.log(body.data[i].practices);
-          $(".showDoctors").append(`${body.data[i].profile.first_name}` + " " + `${body.data[i].profile.last_name}` + "<br>" + `${body.data[i].practices[0].visit_address.street}` + "<br>" + `${body.data[i].practices[0].phones[0].number}` + "<br>");
+          $(".showDoctors").append(`${body.data[i].profile.first_name}` + " " + `${body.data[i].profile.last_name}` + "<br>" + `${body.data[i].practices[0].visit_address.street}` + ", " + `${body.data[i].practices[0].visit_address.state}` + "<br>" + `${body.data[i].practices[0].phones[0].number}` + "<br>");
           if(body.data[i].practices[0].accepts_new_patients === true){
             $(".showDoctors").append("Accepts new patients" + "<br>" + "<br>");
           } else {
             $(".showDoctors").append("Does not accept new patients" + "<br>" + "<br>");
           }
-
         }
       }
     }, function(error){
